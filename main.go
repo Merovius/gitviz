ac89c9052fde44becf2d6d01feaccc46d28e236b
Axel Wagner <mail@merovius.de>
2013-05-21 23:00:58 +0200

    Another commit

--- main.go
+++ main.go
@@ -7,6 +7,43 @@ import (
 	"log"
 )
 
+type Blob struct{
+	*git.Blob
+	id *git.Oid
+}
+
+type Tree struct {
+	*git.Tree
+	id *git.Oid
+	entries []*git.TreeEntry
+}
+
+type Commit struct {
+	*git.Commit
+	id *git.Oid
+	tree *git.Oid
+}
+
+var shorten int
+
+func (b *Blob) String() string {
+	return fmt.Sprintf("\"%s\" [shape=plaintext]", b.id.String()[:shorten])
+}
+
+func (t *Tree) String() string {
+	s := fmt.Sprintf("\"%s\" [shape=oval,style=filled,fillcolor=\"#99ff99\"]", t.id.String()[:shorten])
+	for _, e := range(t.entries) {
+		s += fmt.Sprintf("\n\"%s\" -> \"%s\" [label=\"%s\"]", t.id.String()[:shorten], e.Id.String()[:shorten], e.Name)
+	}
+	return s
+}
+
+func (c *Commit) String() string {
+	s := fmt.Sprintf("\"%s\" [shape=hexagon,style=filled,fillcolor=\"#ffff99\"]\n", c.id.String()[:shorten])
+	s += fmt.Sprintf("\"%s\" -> \"%s\"", c.id.String()[:shorten], c.tree.String()[:shorten])
+	return s
+}
+
 func main () {
 	flag.Parse()
 	var dir string
@@ -29,9 +66,42 @@ func main () {
 		log.Fatal(err)
 	}
 
-	ch := odb.ForEach()
+	stuff := make(map[string]fmt.Stringer)
+	var oids []*git.Oid
 
-	for oid := range(ch) {
-		fmt.Println(oid)
+	for oid := range(odb.ForEach()) {
+		obj, err := repo.Lookup(oid)
+		if err != nil {
+			log.Fatal(err)
+		}
+		switch obj := obj.(type) {
+		default:
+		case *git.Blob:
+			bl := &Blob{ obj, oid }
+			stuff[oid.String()] = bl
+			oids = append(oids, oid)
+		case *git.Tree:
+			tr := &Tree{ obj, oid, nil }
+			stuff[oid.String()] = tr
+			for i := uint64(0); i < tr.EntryCount(); i++ {
+				tr.entries = append(tr.entries, tr.EntryByIndex(i))
+			}
+			oids = append(oids, oid)
+		case *git.Commit:
+			co := &Commit{ obj, oid, obj.TreeId() }
+			stuff[oid.String()] = co
+			oids = append(oids, oid)
+		}
+
 	}
+	shorten, err = git.ShortenOids(oids, 4)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("digraph G {")
+	for _, str := range(stuff) {
+		fmt.Println(str.String())
+	}
+	fmt.Println("}")
 }
